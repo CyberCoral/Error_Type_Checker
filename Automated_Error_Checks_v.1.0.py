ee571ed382ed9eccfefd274c958c84542a49b972
CyberCoral <146016829+CyberCoral@users.noreply.github.com>
2023-11-26 23:14:31 +0100

    Update to Automated_Error_Checks_v.1.0.py
    - Added ConditionCheck() and its automated version ( AutomatedConditionCheck() ), which checks if var (the variable) fits a condition described by the user, which can be of any length.
    
    Also, depending on the severity_mode, it can return different "False" results, such as returning False or a ValueError.

--- Automated_Error_Checks_v.1.0.py
+++ Automated_Error_Checks_v.1.0.py
@@ -1,11 +1,13 @@
 
-# ver. Tue/21/Nov/2023
+# ver. Sun/26/Nov/2023
 #
 # Made by: CyberCoral
 # ------------------------------------------------
 # Github:
 # https://www.github.com/CyberCoral
 #
+
+import re
 
 ###
 ### This program makes a truth list out of a binary string of characters (made of 0s and 1s)
@@ -107,6 +109,69 @@ def ErrorTypeFinder(var, conditions):
     return True
 
 ###
+### This program checks for conditions on the variable var with 2 different severity_mode types.
+###
+def ConditionCheck(var, conditions: str, severity_mode: int = 1):
+    '''
+    The function checks for str conditions
+    and if they are correct or not.
+    Also, depending on severity_mode, it can
+    return different types of errors:
+    
+    - severity_mode = 1: False if the conditions
+    are not met.
+
+    - severity_mode = 2: SyntaxError if the
+    conditions are not met.
+
+    The structure of conditions is the next one:
+    "<condition> & <condition>" for "and" structures.
+    "<condition> | <condition>" for "or" structures.
+
+    All the conditions must have "var" in them.
+    '''
+
+    if isinstance(conditions, str) != True:
+        raise TypeError("conditions must be a str.")
+    elif re.search(".*var",conditions) == None:
+        raise SyntaxError("conditions must contain var at least once.")
+
+    if isinstance(severity_mode, int) != True:
+        raise TypeError("severity_mode must be an int.")
+
+    c = "".join([str(i) for i in conditions]).replace("&"," and ").replace("|"," or ")
+    l = []
+    
+
+    match severity_mode:
+        
+        case 1:
+
+            try:
+                exec(compile(f"b = {c}\nl.append(b)","<string>","exec"))
+                b = l[0]
+                if b == False:
+                    return False
+                return True
+            except SyntaxError:
+                return False
+
+        case 2:
+            
+            try:
+                exec(compile(f"b = {c}\nl.append(b)","<string>","exec"))
+                b = l[0]
+                if b == False:
+                    raise ValueError(f"The conditions ({c}) are not met.")
+                return True
+            except SyntaxError:
+                raise SyntaxError(f"The conditions ({c}) do not make sense, they raise SyntaxError.")
+
+        case _:
+
+            raise OSError(f"This severity mode ({severity_mode}) is not included in the program.") 
+
+###
 ### This program makes an
 ### automated conversion of truth lists out of a binary string of characters (made of 0s and 1s)
 ###
@@ -127,10 +192,10 @@ def AutomatedBinaryToTruthList(binaries: list):
 
     return a
             
-#
-# This program makes an
-# automated check for common variable types on variables.
-#
+###
+### This program makes an
+### automated check for common variable types on variables.
+###
 
 def AutomatedErrorTypeFinder(variables: list, condition_batch: list):
     '''
@@ -157,3 +222,43 @@ def AutomatedErrorTypeFinder(variables: list, condition_batch: list):
         ErrorTypeFinder(variables[i], condition)
 
     return True
+
+###
+### This program makes an
+### automated check for conditions on variables with 2 different types of severity_modes.
+###
+def AutomatedConditionCheck(variables: list, condition_batch: list, severity_modes: list):
+    '''
+    This program automatically checks for
+    conditions in variables with
+    condition_batch and with
+    severity_modes.
+    It follows the same rules of
+    ConditionCheck().
+    '''
+    if isinstance(variables, list) != True:
+        raise TypeError("variables must be a list")
+    elif isinstance(condition_batch, list) != True:
+        raise TypeError("condition_batch must be a list")
+    elif isinstance(severity_modes, list) != True:
+        raise TypeError("severity_modes must be a list")
+
+    if len(variables) != len(condition_batch) and len(variables) != len(severity_modes):
+        raise SyntaxError("There must be the same number of elements for variables and condition_batch ({}).".format(len(variables)))
+
+    for i in range(len(condition_batch)):
+        if isinstance(condition_batch[i], str) != True:
+            raise TypeError("conditions must be a str.")
+        elif re.search(".*var",condition_batch[i]) == None:
+            raise SyntaxError("conditions must contain var at least once.")
+
+    for i in range(len(severity_modes)):
+        if isinstance(severity_modes[i], int) != True:
+            raise TypeError("severity_mode must be an int.")
+
+    results = []
+
+    for i in range(len(variables)):
+        results.append(ConditionCheck(variables[i], condition_batch[i], severity_modes[i]))
+
+    return results
