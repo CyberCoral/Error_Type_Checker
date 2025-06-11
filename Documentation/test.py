4eebe1b8369c23fdaee3ad52c60772727ee7eede
CyberCoral <146016829+CyberCoral@users.noreply.github.com>
2025-06-11 18:19:46 +0200

    Delete Documentation directory
    Delete Documentation directory to update it to latest version of the main program

--- Documentation/test.py
+++
@@ -1,9 +0,0 @@
-import Automated_Error_Checks as AEC # Shorten the name.
-		
-def natural_inv(number):
-	AEC.AutomatedErrorTypeFinder([number],[1]) # number must be an int.
-	AEC.AutomatedConditionCheck([number],["var != 0"],[2]) # For edge case 0.
-	return 1 / number
-		
-a = natural_inv(2)
-b = natural_inv(0) # return ValueError
